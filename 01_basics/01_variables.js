3466b0aaa88c9a8baec842a5755a62837bcf7042
Roman Lamichhane <146808459+RomanLamichhane@users.noreply.github.com>
2023-11-07 08:34:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=44
+let accountEmail="roman@gmail.com"
+var accountPassword="roman@445"
+accountCity="Bharatpur"
+let accountstate;
+
+// accountId=42 // Not allowed 
+accountEmail="Roman554@gmail.com"
+accountPassword="858585"
+accountCity="Kathmandu"
+console.log(accountId);
+/*
+prefer not to use Var because of issues in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
