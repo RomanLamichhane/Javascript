d4e5d06f0d9c47a52dcfd24ef3b1bbb5632fbbe4
Roman Lamichhane <146808459+RomanLamichhane@users.noreply.github.com>
2023-11-08 07:08:59 +0000

    Opeartions

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -24,5 +24,27 @@ let booleanisloggedin = Boolean(isloggedin)
 let someNumber = 33
 
 let stringnumber= String(someNumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ***** OPerations ***** //
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+
+let str1="Roman"
+let str2="Lamichhane"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3=2 +2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
