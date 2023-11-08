1d7ecc332a1d30567fbaf30879f1b79588c764e8
Roman Lamichhane <146808459+RomanLamichhane@users.noreply.github.com>
2023-11-08 06:42:19 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,28 @@
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedin = "Roman"
+
+let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+
+// 1 => true , 0 => False
+// ""=> False
+// "Roman" => True
+
+let someNumber = 33
+
+let stringnumber= String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
